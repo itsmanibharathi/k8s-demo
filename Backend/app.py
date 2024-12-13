b071861e9a436551d1f95996e60d230bafbcf804
Manibharathi <manibharathidct@gmail.com>
2024-12-13 12:27:19 +0530

    create bug

--- Backend/app.py
+++ Backend/app.py
@@ -36,6 +36,7 @@ def get_data():
             host=os.environ['DB_HOST'],
             user=os.environ['DB_USER'],
             password=os.environ['DB_PASSWORD'],
+            # password=os.environ['DB_PASSWORD'],
             database=os.environ['DB_NAME']
         )
 
